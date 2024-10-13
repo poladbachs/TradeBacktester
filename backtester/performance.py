5c6a05c30a64ee664e8e27bbc858b779cfa7c2b5
Polad <bakhip@usi.ch>
2024-10-14 01:33:29 +0200

    annualized return calculated

--- backtester/performance.py
+++ backtester/performance.py
@@ -6,3 +6,6 @@ def calculate_total_return(final_portfolio_value, initial_capital):
     """Calculate the total return of the portfolio."""
     return (final_portfolio_value / initial_capital) - 1
 
+def calculate_annualized_return(total_return, num_days):
+    """Calculate the annualized return of the portfolio."""
+    return np.power((1 + total_return), 252 / num_days) - 1
