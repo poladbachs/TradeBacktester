3567ddbd3d8c2c3583c89095d0e5278ac7fcda55
Polad <bakhip@usi.ch>
2024-10-14 01:40:30 +0200

    added calculate annualized volatility

--- backtester/performance.py
+++ backtester/performance.py
@@ -9,3 +9,7 @@ def calculate_total_return(final_portfolio_value, initial_capital):
 def calculate_annualized_return(total_return, num_days):
     """Calculate the annualized return of the portfolio."""
     return np.power((1 + total_return), 252 / num_days) - 1
+
+def calculate_annualized_volatility(daily_returns):
+    """Calculate the annualized volatility of the portfolio."""
+    return daily_returns.std() * np.sqrt(252)
