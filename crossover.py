4222cbc3c4788af22a97cab330dfff888ccce5dc
Polad <bakhip@usi.ch>
2024-10-14 23:25:49 +0200

    crossover trading strategy starting

---
+++ crossover.py
@@ -0,0 +1,7 @@
+from backtester.data_handler import DataHandler
+from backtester.backtester import Backtester
+from backtester.strategies import Strategy
+
+symbol = "AAPL,MSFT"
+start_date = "2023-01-01"
+end_date = "2023-12-31"
