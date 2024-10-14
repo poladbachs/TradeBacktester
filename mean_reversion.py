fc8d466d1d31a1ecab92d14023bdb07041c178d8
Polad <bakhip@usi.ch>
2024-10-15 01:22:29 +0200

    strategy indicators defined for mean reversion

--- mean_reversion.py
+++ mean_reversion.py
@@ -7,3 +7,12 @@ start_date = "2022-01-01"
 end_date = "2022-12-31"
 
 data = DataHandler(symbol=symbol, start_date=start_date, end_date=end_date).load_data()
+
+strategy = Strategy(
+    indicators={
+        "sma_50": lambda row: row["close"].rolling(window=50).mean(),
+        "std_3": lambda row: row["close"].rolling(window=50).std() * 3,
+        "std_3_upper": lambda row: row["sma_50"] + row["std_3"],
+        "std_3_lower": lambda row: row["sma_50"] - row["std_3"],
+    },
+)
