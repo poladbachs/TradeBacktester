85f03c84e169549e4122523c27f1c5dc00148ef3
Polad <bakhip@usi.ch>
2024-10-15 03:21:42 +0200

    pairs trade starting

---
+++ pairs_trade.py
@@ -0,0 +1,8 @@
+from backtester.data_handler import DataHandler
+from backtester.backtester import Backtester
+from backtester.strategies import Strategy
+
+symbol = "NFLX,ROKU"
+start_date = "2023-01-01"
+
+import pandas as pd
