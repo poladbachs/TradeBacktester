7dcc582079a23e84308de7927f1afe1ef4b0b288
Polad <bakhip@usi.ch>
2024-10-15 03:33:52 +0200

    pairs trade strategy defined, indicators and signal logic added

--- pairs_trade.py
+++ pairs_trade.py
@@ -22,3 +22,15 @@ data = pd.merge(
 
 data = data.rename(columns={"close_ROKU": "close"})
 data.head()
+
+strategy = Strategy(
+    indicators={
+        "day_5_lookback_NFLX": lambda row: row["close_NFLX"].shift(5),
+        "day_5_lookback_ROKU": lambda row: row["close"].shift(5),
+    },
+    signal_logic=lambda row: (
+        -1
+        if row["close_NFLX"] > row["day_5_lookback_NFLX"] * 1.05
+        else 1 if row["close_NFLX"] < row["day_5_lookback_NFLX"] * 0.95 else 0
+    ),
+)
