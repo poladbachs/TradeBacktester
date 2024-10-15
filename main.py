b2e088805cf8e6cbe52297def268233ecc44d863
Polad <bakhip@usi.ch>
2024-10-15 04:27:19 +0200

    strategy with indicators and signal logic added and data defined

--- main.py
+++ main.py
@@ -12,4 +12,12 @@ def main():
         symbol=symbol, start_date=start_date, end_date=end_date
     ).load_data()
 
-    
+    strategy = Strategy(
+        indicators={
+            "sma_20": lambda row: row["close"].rolling(window=20).mean(),
+            "sma_60": lambda row: row["close"].rolling(window=60).mean(),
+        },
+        signal_logic=lambda row: 1 if row["sma_20"] > row["sma_60"] else -1,
+    )
+
+    data = Strategy.generate_signals(data)
