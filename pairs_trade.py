5372cca9247822ac568c455a47aec0502fedda38
Polad <bakhip@usi.ch>
2024-10-15 03:28:31 +0200

    data for pairs trade defined and handled

--- pairs_trade.py
+++ pairs_trade.py
@@ -6,3 +6,19 @@ symbol = "NFLX,ROKU"
 start_date = "2023-01-01"
 
 import pandas as pd
+
+data = DataHandler(
+    symbol=symbol,
+    start_date=start_date,
+).load_data()
+
+data = pd.merge(
+    data["NFLX"].reset_index(),
+    data["ROKU"].reset_index(),
+    left_index=True,
+    right_index=True,
+    suffixes=("_NFLX", "_ROKU"),
+)
+
+data = data.rename(columns={"close_ROKU": "close"})
+data.head()
