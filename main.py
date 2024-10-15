d40ab177fe471a596003bf12f305a1fdb725851c
Polad <bakhip@usi.ch>
2024-10-15 04:33:45 +0200

    main finished, pretty much all seems ok

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ def main():
     start_date = "2023-01-01"
     end_date = "2023-12-31"
 
-    data = DataHandler(
-        symbol=symbol, start_date=start_date, end_date=end_date
-    ).load_data()
+    data = DataHandler(symbol=symbol, start_date=start_date, end_date=end_date).load_data()
 
     strategy = Strategy(
         indicators={
@@ -19,5 +17,11 @@ def main():
         },
         signal_logic=lambda row: 1 if row["sma_20"] > row["sma_60"] else -1,
     )
+    data = strategy.generate_signals(data)
 
-    data = Strategy.generate_signals(data)
+    backstester = Backtester()
+    backstester.backtest(data)
+    backstester.calculate_performance()
+
+if __name__ == "__main__":
+    main()
